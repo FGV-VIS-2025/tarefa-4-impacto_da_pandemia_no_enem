b92792b1e259e487e4e6ac7c4aa72e706b5313e2
GuilhermeCastilho02 <guilherme222castilho@gmail.com>
2025-04-28 21:52:15 -0300

    feat: legenda interativa adicionada

--- script.js
+++ script.js
@@ -66,22 +66,16 @@ Promise.all([
         .append("div")
         .attr("class", "tooltip");
 
+    let array;
     // Atualiza o gráfico com base na região selecionada
     function barCharts(region, column, filteredCategory) {
+        let colorScale = d3.schemeCategory10;
+        svg.selectAll(".legend").remove();
         // Filtra os dados conforme a região
         const filteredData2019 = (region === "all") ? data2019 : data2019.filter(d => d.MESORREGIAO === region);
         const filteredData2020 = (region === "all") ? data2020 : data2020.filter(d => d.MESORREGIAO === region);
 
-        const array = [...new Set(filteredData2019.map(d => d[column]))];
-
-        // const dictColumns = {
-        //     "Raça" : "TP_COR_RACA",
-        //     "Sexo" : "TP_SEXO",
-        //     "Escola" : "TP_ESCOLA",
-        //     "Estado Civil" : "TP_ESTADO_CIVIL",
-        //     "Faixa Etária" : "TP_FAIXA_ETARIA",
-        //     "Zona" : "TP_LOCALIZACAO_ESC",
-        // };
+        array = [...new Set(filteredData2019.map(d => d[column]))];
 
         const subscriptions = [
                         {year: "2019", total: filteredData2019.length},
@@ -139,19 +133,73 @@ Promise.all([
             .attr("y", d => y(d.value))
             .attr("width", xSubgroup.bandwidth())
             .attr("height", d => y(0) - y(d.value))
-            .attr("fill", (d, i) => d3.schemeCategory10[i % 10])
+            .attr("fill", (d, i) => colorScale[i % 10])
+            .on("mouseover", function (event, d) {
+                d3.select(this) 
+                .transition()
+                .duration(200)
+                .attr("stroke", "black")
+            })
+            .on("mouseout", function (event, d) {
+                d3.select(this) 
+                    .transition()
+                    .duration(200)
+                    .attr("stroke", "none")
+            })
             .on("click", function (event, d){
                 barCharts(region, column, array);
             });
+
         // Remove barras excedentes, se existirem
         barGroups.exit().remove();
+
+        createLegend(colorScale);
     }
 
-    barCharts("all", "TP_COR_RACA");
+    function createLegend(colorScale) {
+        // Adiciona a legenda ao gráfico
+        const legend = svg.append("g")
+            .attr("class", "legend")
+            .attr("transform", `translate(${width - margin.right - 100},${margin.top})`);
+        
+        if (array.length === 1) {
+            svg.selectAll(".legend").remove();
+        }
+        else {
+            array.forEach((category, i) => {
+                const legendGroup = legend.append("g")
+                    .attr("class", "legend-item") // Classe para cada item da legenda
+                    .attr("transform", `translate(0, ${i * 20})`);
 
-    // Configuração do botão "Reset Filter" – este botão é livre e posicionado conforme o CSS
+                legendGroup.append("rect")
+                    .attr("class", "legend-rect") // Classe para os retângulos
+                    .attr("width", 15)
+                    .attr("height", 15)
+                    .attr("fill", colorScale[i % 10]);
+                
+                    legendGroup.append("text")
+                        .attr("class", "legend-text") // Classe para os textos
+                        .attr("x", 20)
+                        .attr("y", 12)
+                        .text(category)
+                        .style("font-size", "12px")
+                        .attr("fill", "black");
+                // Vincula os dados às legendas
+                legendGroup.datum(category);
+            });
+        }
+        legend.selectAll(".legend-item")
+            .on("click", function(event, d) {
+                console.log(`Categoria clicada: ${d}`);
+                barCharts("all", "TP_COR_RACA", d);
+            });
+    }
+
+    barCharts("all", "TP_SEXO");
+
+    // Configuração do botão "Remover Filtros"
     d3.select("#reset-button").on("click", () => {
-        barCharts("all", "TP_COR_RACA");
+        barCharts("all", "TP_SEXO");
         svgMap.selectAll("path").classed("selected", false).attr("fill", "#69b3a2");
     });
     
@@ -188,13 +236,14 @@ Promise.all([
                 d3.select(this).attr("fill", "green");
                 tooltip.transition().style("opacity", 1);
                 tooltip.html(`<strong>${d.properties.nm_meso}</strong>`)
-                .style("left", `${event.pageX + 5}px`)
-                .style("top", `${event.pageY - 5}px`);
+                    .style("left", `${event.pageX + 5}px`)
+                    .style("top", `${event.pageY - 5}px`);
             })
             .on("mouseout", function (event, d) {
                 if (!d3.select(this).classed("selected")) {
                     d3.select(this).attr("fill", "#69b3a2");
                 }
+                else {d3.select(this).attr("fill", "darkgreen");}
                 tooltip.transition().style("opacity", 0);
             })
             .on("click", function (event, d){
@@ -212,5 +261,3 @@ Promise.all([
     });
 
 });
-  
-  
