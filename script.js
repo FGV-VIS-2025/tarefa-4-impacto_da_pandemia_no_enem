cca91c0b909188520329ce31701f0fb8015644c6
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2025-05-02 07:08:33 -0300

    fix: Melhorias e concertos no heatmap
    - As cores no heatmap agora são mostradas corretamente
    - Gradiente agora começa em um tom de azul claro ao invés de branco
    - Melhorias de visualização em dispositivos móveis

--- script.js
+++ script.js
@@ -530,18 +530,8 @@ Promise.all([
             .call(axis);
     }
 
-    function updateHeatMap(region, year) {
-        // Filtra os dados conforme a região
-        let filteredDataYear;
-        if (year === 2019) {
-            filteredDataYear = data2019;
-        } else if (year === 2020) {
-            filteredDataYear = data2020;
-        } else {
-            console.error("Ano inválido. Use 2019 ou 2020.");
-        }
-
-        const filteredData = (region === "all") ? filteredDataYear : filteredDataYear.filter(d => d.MESORREGIAO === region);
+    function updateHeatMap(region) {
+        const years = [2019, 2020];
 
         // Obtém por meio do select do HTML as variáveis dos eixos
         let selected1 = document.getElementById("variable1").value;
@@ -557,153 +547,189 @@ Promise.all([
         const width = fullWidth - marginHeatmap.left - marginHeatmap.right;
         const height = fullHeight - marginHeatmap.top - marginHeatmap.bottom;
 
-        // Contagem das combinações de variáveis
-        const counts = {};
-        filteredData.forEach(d => {
+        filteredData2019 = (region === "all") ? data2019 : data2019.filter(d => d.MESORREGIAO === region);
+        filteredData2020 = (region === "all") ? data2020 : data2020.filter(d => d.MESORREGIAO === region);
+
+        let counts2019 = {};
+        filteredData2019.forEach(d => {
             const variable1 = d[selected1];
             const variable2 = d[selected2];
             const key = `${variable1}-${variable2}`
-            counts[key] = (counts[key] || 0) + 1;
+            counts2019[key] = (counts2019[key] || 0) + 1;
         });
 
-        let cats1;
-        let cats2;
-        if (selected1 === "TP_FAIXA_ETARIA") {
-            cats1 = [...new Set(filteredData.map(d => d[selected1]))]
-                                                  .sort((a, b) => parseInt(a) - parseInt(b))
-                                                  .slice(0, 6);
-        }
-        else {
-            cats1 = [...new Set(filteredData.map(d => d[selected1]))].sort();
-        }
-        if (selected2 === "TP_FAIXA_ETARIA") {
-            cats2 = [...new Set(filteredData.map(d => d[selected2]))]
-                                                  .sort((a, b) => parseInt(a) - parseInt(b))
-                                                  .slice(0, 6);          
-        }
-        else {
-            cats2 = [...new Set(filteredData.map(d => d[selected2]))].sort();
-        }
-
-        const map1 = LOOKUP[selected1] || (d=>d);
-        const map2 = LOOKUP[selected2] || (d=>d);
-
-        const fullGrid = [];
-        cats1.forEach(v1 => {
-            cats2.forEach(v2 => {
-                const key = `${v1}-${v2}`;
-                fullGrid.push({
-                    v1,
-                    v2,
-                    value: counts[key] || 0
-                });
-            });
+        let counts2020 = {};
+        filteredData2020.forEach(d => {
+            const variable1 = d[selected1];
+            const variable2 = d[selected2];
+            const key = `${variable1}-${variable2}`
+            counts2020[key] = (counts2020[key] || 0) + 1;
         });
 
-        //  Desenvolvimento do gráfico
-        const x = d3.scaleBand()
-            .domain(cats1)
-            .range([0, width])
-            .padding(0.05);
+        let maxCount2019 = d3.max(Object.values(counts2019));
+        let maxCount2020 = d3.max(Object.values(counts2020));
 
-        const y = d3.scaleBand()
-            .domain(cats2)
-            .range([height, 0])
-            .padding(0.05);
+        const customBlues = t => d3.interpolateBlues(0.1 + 0.9 * t);
 
         const color = d3.scaleSequential()
-            .interpolator(d3.interpolateBlues)
-            .domain([0, d3.max(Object.values(counts))]);
+            .interpolator(customBlues)
+            .domain([0, d3.max(Object.values([maxCount2019, maxCount2020]))]);
+            
+        filteredDataYears = {2019: filteredData2019, 2020: filteredData2020};
 
-        let svgHeatmap;
-        if (year === 2019) {    
-            svgHeatmap = d3.select("#heatmap2019")
-                .attr("width", fullWidth)
-                .attr("height", fullHeight);
-        }
-        else if (year === 2020) {
-            svgHeatmap = d3.select("#heatmap2020")
-                .attr("width", fullWidth)
-                .attr("height", fullHeight);
-        }
+        years.forEach(year => {
+            let cats1;
+            let cats2;
+            if (selected1 === "TP_FAIXA_ETARIA") {
+                cats1 = [...new Set(filteredDataYears[year].map(d => d[selected1]))]
+                                                    .sort((a, b) => parseInt(a) - parseInt(b))
+                                                    .slice(0, 6);
+            }
+            else {
+                cats1 = [...new Set(filteredDataYears[year].map(d => d[selected1]))].sort();
+            }
+            if (selected2 === "TP_FAIXA_ETARIA") {
+                cats2 = [...new Set(filteredDataYears[year].map(d => d[selected2]))]
+                                                    .sort((a, b) => parseInt(a) - parseInt(b))
+                                                    .slice(0, 6);          
+            }
+            else {
+                cats2 = [...new Set(filteredDataYears[year].map(d => d[selected2]))].sort();
+            }
 
-        svgHeatmap.selectAll("*").remove();
-        // Atualiza o eixo Y
-        const g = svgHeatmap.append("g")
-            .attr("transform", `translate(${marginHeatmap.left},${marginHeatmap.top})`);
+            const map1 = LOOKUP[selected1] || (d=>d);
+            const map2 = LOOKUP[selected2] || (d=>d);
 
-        g.selectAll("rect")
-            .data(fullGrid, d => d.v1 + "-" + d.v2)
-            .enter()
-            .append("rect")
-                .attr("x", d => x(d.v1))
-                .attr("y", d => y(d.v2))
-                .attr("width", x.bandwidth() - 1)
-                .attr("height", y.bandwidth() - 1)
-                .attr("fill", d => color(d.value))
-                .on("mouseover", (event, d) => {
-                    const label1 = map1[d.v1] ||  d.v1;
-                    const label2 = map2[d.v2] ||  d.v2;
+            const fullGrid = []
+            if (year === 2019) {
+                cats1.forEach(v1 => {
+                    cats2.forEach(v2 => {
+                        const key = `${v1}-${v2}`;
+                        fullGrid.push({
+                            v1,
+                            v2,
+                            value: counts2019[key] || 0
+                        });
+                    });
+                });
+            }
+            else if (year === 2020) {
+                cats1.forEach(v1 => {
+                    cats2.forEach(v2 => {
+                        const key = `${v1}-${v2}`;
+                        fullGrid.push({
+                            v1,
+                            v2,
+                            value: counts2020[key] || 0
+                        });
+                    });
+                });
+            }
 
-                    tooltip.style("opacity", 1).html(`<strong>${label1}</strong> × <strong>${label2 }</strong><br/><em>${d.value}</em> participante(s)`);
-                })
-                .on("mousemove", (event, d) => {
-                    tooltip.style("left", `${event.pageX + 10}px`).style("top", `${event.pageY - 25}px`);
-                })
-                .on("mouseout", () => {
-                    tooltip.style("opacity", 0);
-                })
+            //  Desenvolvimento do gráfico
+            const x = d3.scaleBand()
+                .domain(cats1)
+                .range([0, width])
+                .padding(0.05);
 
-        
-        // Adiciona os rótulos de texto
-        const xAxis = d3.axisBottom(x).tickFormat(code => map1[code] ?? code);
-        const yAxis = d3.axisLeft(y).tickFormat(code => map2[code] ?? code);
+            const y = d3.scaleBand()
+                .domain(cats2)
+                .range([height, 0])
+                .padding(0.05);
 
-        const selectedText1 = document.getElementById("variable1").options[document.getElementById("variable1").selectedIndex].text;
-        const selectedText2 = document.getElementById("variable2").options[document.getElementById("variable2").selectedIndex].text;
- 
-        g.append("g")
-            .attr("class", "x-axis")
-            .attr("transform", `translate(0,${height})`)
-            .call(xAxis)
-            .selectAll("text")
-                .attr("transform", "rotate(-45)")
-                .attr("dx", "-0.6em")
-                .attr("dy", "0.25em")
-                .style("text-anchor", "end");
+            let svgHeatmap;
+            if (year === 2019) {    
+                svgHeatmap = d3.select("#heatmap2019")
+                    .attr("width", "100%")
+                    .attr("height", "auto")
+                    .attr("viewBox", `-30 0 ${fullWidth} ${fullHeight}`)
+                    .attr("preserveAspectRatio", "xMidYMid meet");
+            }
+            else if (year === 2020) {
+                svgHeatmap = d3.select("#heatmap2020")
+                    .attr("width", "100%")
+                    .attr("height", "auto")
+                    .attr("viewBox", `-30 0 ${fullWidth} ${fullHeight}`)
+                    .attr("preserveAspectRatio", "xMidYMid meet");
 
-        g.append("g")
-            .attr("class", "y-axis")
-            .call(yAxis)
-            .selectAll("text")
-                .attr("dx", "-0.6em")
-                .style("text-anchor", "end");
-                
-        g.append("text")
-            .attr("class","axis-label")
-            .attr("x", width / 2)
-            .attr("y", height + margin.bottom + 30)
-            .attr("text-anchor", "middle")
-            .text(selectedText1);
+            }
 
-        g.append("text")
-            .attr("class","axis-label")
-            .attr("transform", "rotate(-90)")
-            .attr("x", -height / 2)
-            .attr("y", -margin.left - 25)
-            .attr("text-anchor", "middle")
-            .text(selectedText2);
+            svgHeatmap.selectAll("*").remove();
+            // Atualiza o eixo Y
+            const g = svgHeatmap.append("g")
+                .attr("transform", `translate(${marginHeatmap.left},${marginHeatmap.top})`);
 
-        updateColorbar(color);
+            g.selectAll("rect")
+                .data(fullGrid, d => d.v1 + "-" + d.v2)
+                .enter()
+                .append("rect")
+                    .attr("x", d => x(d.v1))
+                    .attr("y", d => y(d.v2))
+                    .attr("width", x.bandwidth() - 1)
+                    .attr("height", y.bandwidth() - 1)
+                    .attr("fill", d => color(d.value))
+                    .on("mouseover", (event, d) => {
+                        const label1 = map1[d.v1] ||  d.v1;
+                        const label2 = map2[d.v2] ||  d.v2;
 
+                        tooltip.style("opacity", 1).html(`<strong>${label1}</strong> × <strong>${label2 }</strong><br/><em>${d.value}</em> participante(s)`);
+                    })
+                    .on("mousemove", (event, d) => {
+                        tooltip.style("left", `${event.pageX + 10}px`).style("top", `${event.pageY - 25}px`);
+                    })
+                    .on("mouseout", () => {
+                        tooltip.style("opacity", 0);
+                    })
+
+            
+            // Adiciona os rótulos de texto
+            const xAxis = d3.axisBottom(x).tickFormat(code => map1[code] ?? code);
+            const yAxis = d3.axisLeft(y).tickFormat(code => map2[code] ?? code);
+
+            const selectedText1 = document.getElementById("variable1").options[document.getElementById("variable1").selectedIndex].text;
+            const selectedText2 = document.getElementById("variable2").options[document.getElementById("variable2").selectedIndex].text;
+    
+            g.append("g")
+                .attr("class", "x-axis")
+                .attr("transform", `translate(0,${height})`)
+                .call(xAxis)
+                .selectAll("text")
+                    .attr("transform", "rotate(-45)")
+                    .attr("dx", "-0.6em")
+                    .attr("dy", "0.25em")
+                    .style("text-anchor", "end");
+
+            g.append("g")
+                .attr("class", "y-axis")
+                .call(yAxis)
+                .selectAll("text")
+                    .attr("dx", "-0.6em")
+                    .style("text-anchor", "end");
+                    
+            g.append("text")
+                .attr("class","axis-label")
+                .attr("x", width / 2)
+                .attr("y", height + margin.bottom + 30)
+                .attr("text-anchor", "middle")
+                .text(selectedText1);
+
+            g.append("text")
+                .attr("class","axis-label")
+                .attr("transform", "rotate(-90)")
+                .attr("x", -height / 2)
+                .attr("y", -margin.left - 25)
+                .attr("text-anchor", "middle")
+                .text(selectedText2);
+            })
+
+            updateColorbar(color);
     }
 
     let currentRegion = "all";
 
     barCharts("all"); 
     boxPlot("all");
-    updateHeatMap("all", 2019);
-    updateHeatMap("all", 2020);
+    updateHeatMap("all");
     
 
     d3.select("#select-button").on("change", () => {
@@ -721,20 +747,20 @@ Promise.all([
         document.getElementById("variable1").selectedIndex = 0;
         document.getElementById("variable2").selectedIndex = 0;
 
-        updateHeatMap("all", 2019);
-        updateHeatMap("all", 2020);
+        updateHeatMap("all");
+        updateHeatMap("all");
         
     });
     
     d3.select("#reset-button").style("display", "block");
 
     d3.select("#variable1").on("change", () => {
-        updateHeatMap(currentRegion, 2019); 
-        updateHeatMap(currentRegion, 2020);
+        updateHeatMap(currentRegion); 
+        updateHeatMap(currentRegion);
     });
     d3.select("#variable2").on("change", () => {
-        updateHeatMap(currentRegion, 2019); 
-        updateHeatMap(currentRegion, 2020);
+        updateHeatMap(currentRegion); 
+        updateHeatMap(currentRegion);
     });
 
     const containerMap = d3.select("#map-container")
